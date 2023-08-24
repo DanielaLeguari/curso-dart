08b3e16a4bd539eb25ec5fc39be6aa1de8204127
Daniela Leguari <danielaleguari@outlook.com>
2023-08-24 15:39:19 -0300

    projeto inicial

---
+++ bin/dart_curso2.dart
@@ -0,0 +1,4 @@
+
+void main() {
+  print("Oi Mundo!");
+}
---
+++ test/dart_curso2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_curso2/dart_curso2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
