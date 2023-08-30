2a52afdce73da4639f6b7af81e3a544d9898410a
Daniela Leguari <danielaleguari@outlook.com>
2023-08-30 15:16:34 -0300

    criando funções

--- bin/dart_curso2.dart
+++ bin/dart_curso2.dart
@@ -1,9 +1,18 @@
-
 void main() {
   String nome = "Laranja";
   double peso = 100.2;
   String cor = "Verde e Amarela";
   String sabor = "Doce e cítrica";
   int diasDesdeColheita = 40;
-  bool isMadura;
+  bool isMadura = funcEstaMadura(diasDesdeColheita);
+
+  print(isMadura);
+}
+
+bool funcEstaMadura(int dias) {
+  if (dias >= 30) {
+    return true;
+  } else {
+    return false;
+  }
 }
