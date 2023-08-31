350a905fc882cde8dd331a57e23a7c6c7f228e53
Daniela Leguari <danielaleguari@outlook.com>
2023-08-31 15:12:12 -0300

    metodos

--- bin/dart_curso2.dart
+++ bin/dart_curso2.dart
@@ -5,26 +5,20 @@ void main() {
   String sabor = "Doce e cítrica";
   int diasDesdeColheita = 40;
   bool isMadura;
-                        //argumentos
-  Fruta fruta1 = Fruta(nome, peso, cor, sabor, diasDesdeColheita); // instancia
-  Fruta fruta2 = Fruta("Uva", 40, "roxo", "doce", 35);
-
-  print(fruta1.nome);
-  print(fruta2.nome);
+ 
+  if(diasDesdeColheita >= 30){
+    isMadura= true;
+  } else {
+    isMadura= false;
+  }
 }
 
-class Fruta {
-
-//propriedades -> caracteristicas do objeto
-  String nome;
-  double peso;
-  String cor;
-  String sabor;
-  int diasDesdeColheita;
-  bool? isMadura;
-
-//construtor-> método especial
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
+bool funcEstaMadura(int dias) {
+  if (dias >= 30) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 int funcQuantosDiasmadura( int dias){
@@ -32,7 +26,6 @@ int funcQuantosDiasmadura( int dias){
   int quantosDiasFalta = diasParaMadura - dias;
   return quantosDiasFalta;
 }
-
                                             //parametros nomeados opcionais {}
 void mostrarMadura(String nome, int dias, {required String cor }) {
   if (dias >= 30) {
@@ -46,10 +39,21 @@ void mostrarMadura(String nome, int dias, {required String cor }) {
   }
 }
 
-bool funcEstaMadura(int dias) {
-  if (dias >= 30) {
-    return true;
-  } else {
-    return false;
+class Fruta {
+//propriedades -> caracteristicas do objeto
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
+
+//construtor-> método especial
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
+
+  //metodo
+  void estaMadura(int diasParaMadura){
+    isMadura = diasDesdeColheita >= diasParaMadura;
+    print("A sua $nome foi colhida há $diasDesdeColheita dias, e precisa de $diasParaMadura para poder comer. Ela está madura? $isMadura.");
   }
 }
